e45ec424b0d28bddfab033db2b9b832d73007cdf
Wataru Kay <watarukay@gmail.com>
2018-11-16 19:58:09 +0900

    Updates

---
+++ precache-manifest.e83a9916ea1dd30d3591891ca23d1864.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "461bb02d5d921ccc3556950112453dc9",
+    "url": "/hoopfeed/static/media/ball.461bb02d.svg"
+  },
+  {
+    "revision": "4e1c36367ecb3fa034dca8b731c6abf8",
+    "url": "/hoopfeed/static/media/SF-Pro-Text-Medium.4e1c3636.otf"
+  },
+  {
+    "revision": "7e3a0b3e6150a38f82238bdbc2e0ab6e",
+    "url": "/hoopfeed/static/media/SF-Pro-Text-Heavy.7e3a0b3e.otf"
+  },
+  {
+    "revision": "18f171a80125ae69eae06d7334468941",
+    "url": "/hoopfeed/static/media/FugazOne-Regular.18f171a8.ttf"
+  },
+  {
+    "revision": "6251f99ec4ccc35ba152",
+    "url": "/hoopfeed/static/js/runtime~main.6251f99e.js"
+  },
+  {
+    "revision": "89f827c579f584491443",
+    "url": "/hoopfeed/static/js/main.89f827c5.chunk.js"
+  },
+  {
+    "revision": "98c7108c793a1867f9b4",
+    "url": "/hoopfeed/static/js/1.98c7108c.chunk.js"
+  },
+  {
+    "revision": "89f827c579f584491443",
+    "url": "/hoopfeed/static/css/main.a515f053.chunk.css"
+  },
+  {
+    "revision": "b2399989ed9ca3f8f92b5fef472fcb17",
+    "url": "/hoopfeed/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/hoopfeed/precache-manifest.e83a9916ea1dd30d3591891ca23d1864.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/hoopfeed/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
