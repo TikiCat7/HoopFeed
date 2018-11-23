097778c5e624d7baff010f51791fe83372c8a152
Wataru Kay <watarukay@gmail.com>
2018-11-23 12:56:57 +0900

    Add apoll-cache to local storage

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,11 @@
 import React, { useState } from 'react';
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
-
 import AppContext from '../context/AppContext';
 import VideoContext from '../context/VideoContext';
 import Header from './Header';
 import Content from './Content';
 
-const client = new ApolloClient({
-  link: new HttpLink({
-    uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
-  }),
-  cache: new InMemoryCache(),
-});
-
-const App = () => {
+const App = ({ client }) => {
   const [showVideoOverlay, toggleVideoOverlay] = useState(false);
   const [selectedVideo, setVideoId] = useState(null);
   const [videoPlaying, toggleVideoPlay] = useState(false);
@@ -46,7 +35,7 @@ const App = () => {
             setSelectedMatchVideos,
           }}
         >
-          {!showVideoOverlay && <Header />}
+          <Header />
           <Content />
         </VideoContext.Provider>
       </AppContext.Provider>
--- src/index.js
+++ src/index.js
@@ -3,14 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { persistCache } from 'apollo-cache-persist';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const setupApollo = async () => {
+  let cache = new InMemoryCache();
+  await persistCache({
+    cache,
+    storage: window.localStorage,
+  });
+  let client = new ApolloClient({
+    link: new HttpLink({
+      uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+    }),
+    cache: cache,
+  });
+  return client;
+};
 
-if (module.hot) {
-  module.hot.accept();
+async function renderApp() {
+  let client = await setupApollo();
+  ReactDOM.render(<App client={client} />, document.getElementById('root'));
+
+  if (module.hot) {
+    module.hot.accept();
+  }
+  serviceWorker.register();
 }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+renderApp();
